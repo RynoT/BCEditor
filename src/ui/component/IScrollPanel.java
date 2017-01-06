7031fa36645f1d82dc28dc7e6ab847305911ad9f
Ryan Thomson <1502306@abertay.ac.uk>
2017-01-06 20:58:21 +0000

    Scroll bar now accomodates horizontal scrolling better

--- src/ui/component/IScrollPanel.java
+++ src/ui/component/IScrollPanel.java
@@ -1,8 +1,6 @@
 package ui.component;
 
 import javax.swing.*;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
 import java.awt.*;
 import java.awt.event.*;
 
@@ -48,7 +46,7 @@ public class IScrollPanel extends IComponent {
         if(vertical) {
             super.add(this.vertical, BorderLayout.EAST);
 
-            content.addMouseWheelListener(e -> IScrollPanel.this.vertical.move((int)(e
+            content.addMouseWheelListener(e -> IScrollPanel.this.vertical.move((int) (e
                     .getUnitsToScroll() * IScrollPanel.this.vertical.getWheelMultiplier())));
         }
         this.updateScale();
@@ -72,32 +70,32 @@ public class IScrollPanel extends IComponent {
         });
     }
 
-    public JViewport getViewport(){
+    public JViewport getViewport() {
         return this.viewport;
     }
 
     private void updateScale() {
         final Dimension target = this.content.getPreferredSize();
         if(this.vertical != null) {
-            if(target.height == 0 || super.getHeight() >= target.height){
+            if(target.height == 0 || super.getHeight() >= target.height) {
                 this.vertical.y = 0;
                 this.vertical.height = -1;
             } else {
                 this.vertical.height = Math.max(IScrollPanel.SCROLLER_MIN_SCALE,
                         super.getHeight() * super.getHeight() / target.height);
-                this.vertical.y = (int)(super.getHeight() * this.viewport.getViewPosition().y / (float)target.height);
+                this.vertical.y = (int) (super.getHeight() * this.viewport.getViewPosition().y / (float) target.height);
             }
             this.vertical.limitPosition();
             this.vertical.repaint();
         }
-        if(this.horizontal != null){
-            if(target.width == 0 || super.getWidth() >= target.width){
+        if(this.horizontal != null) {
+            if(target.width == 0 || super.getWidth() >= target.width) {
                 this.horizontal.x = 0;
                 this.horizontal.width = -1;
             } else {
                 this.horizontal.width = Math.max(IScrollPanel.SCROLLER_MIN_SCALE,
                         super.getWidth() * super.getWidth() / target.width);
-                this.horizontal.x = (int)(super.getWidth() * this.viewport.getViewPosition().x / (float)target.width);
+                this.horizontal.x = (int) (super.getWidth() * this.viewport.getViewPosition().x / (float) target.width);
             }
             this.horizontal.limitPosition();
             this.horizontal.repaint();
@@ -109,7 +107,7 @@ public class IScrollPanel extends IComponent {
         private final boolean horizontal;
 
         private int x = 0, y = 0, width, height;
-        private boolean hovered = false, scrolling = false;
+        private boolean backHover = false, barHover = false, scrolling = false;
 
         private IScroller(final boolean horizontal, final int size) {
             this.horizontal = horizontal;
@@ -127,16 +125,21 @@ public class IScrollPanel extends IComponent {
                 private Point press;
 
                 @Override
+                public void mouseEntered(final MouseEvent e) {
+                    IScroller.this.backHover = true;
+                    IScroller.super.repaint();
+                }
+
+                @Override
                 public void mouseExited(final MouseEvent e) {
-                    if(IScroller.this.hovered) {
-                        IScroller.this.hovered = false;
-                        IScroller.super.repaint();
-                    }
+                    IScroller.this.backHover = false;
+                    IScroller.this.barHover = false;
+                    IScroller.super.repaint();
                 }
 
                 @Override
                 public void mousePressed(final MouseEvent e) {
-                    if(this.isHovered(e.getPoint())){
+                    if(this.isBarHovered(e.getPoint())) {
                         IScroller.this.scrolling = true;
                         this.press = e.getLocationOnScreen();
                     }
@@ -150,16 +153,16 @@ public class IScrollPanel extends IComponent {
 
                 @Override
                 public void mouseMoved(final MouseEvent e) {
-                    final boolean hovered = this.isHovered(e.getPoint());
-                    if(IScroller.this.hovered != hovered) {
-                        IScroller.this.hovered = hovered;
+                    final boolean hovered = this.isBarHovered(e.getPoint());
+                    if(IScroller.this.barHover != hovered) {
+                        IScroller.this.barHover = hovered;
                         IScroller.super.repaint();
                     }
                 }
 
                 @Override
                 public void mouseDragged(final MouseEvent e) {
-                    if(!IScroller.this.scrolling){
+                    if(!IScroller.this.scrolling) {
                         return;
                     }
                     final int offset;
@@ -174,10 +177,10 @@ public class IScrollPanel extends IComponent {
 
                 @Override
                 public void mouseWheelMoved(final MouseWheelEvent e) {
-                    IScroller.this.move((int)(e.getUnitsToScroll() * IScroller.this.getWheelMultiplier()));
+                    IScroller.this.move((int) (e.getUnitsToScroll() * IScroller.this.getWheelMultiplier()));
                 }
 
-                private boolean isHovered(final Point p){
+                private boolean isBarHovered(final Point p) {
                     // Check to see if mouse is on top of scroll bar
                     return p.getX() >= IScroller.this.x && p.getX() < IScroller.this.x + IScroller.this.width
                             && p.getY() >= IScroller.this.y && p.getY() < IScroller.this.y + IScroller.this.height;
@@ -188,13 +191,13 @@ public class IScrollPanel extends IComponent {
             super.addMouseMotionListener(mouse);
         }
 
-        private float getWheelMultiplier(){
+        private float getWheelMultiplier() {
             final float multiplier = super.getHeight() * 0.001f;
             return multiplier > 1.0f ? IScrollPanel.SCROLLER_WHEEL_SENSITIVITY : IScrollPanel.SCROLLER_WHEEL_SENSITIVITY * multiplier;
         }
 
-        private void move(final int offset){
-            if(this.horizontal){
+        private void move(final int offset) {
+            if(this.horizontal) {
                 this.x += offset;
             } else {
                 this.y += offset;
@@ -204,22 +207,30 @@ public class IScrollPanel extends IComponent {
             IScroller.super.repaint();
         }
 
-        private void limitPosition(){
-            if(this.horizontal){
-                this.x = Math.max(0, Math.min(IScrollPanel.super.getWidth() - this.width, this.x));
+        private void limitPosition() {
+            if(this.horizontal) {
+                int width = IScrollPanel.super.getWidth();
+                if(IScrollPanel.this.vertical != null) {
+                    width -= IScrollPanel.this.vertical.getWidth();
+                }
+                this.x = Math.max(0, Math.min(width - this.width, this.x));
             } else {
-                this.y = Math.max(0, Math.min(IScrollPanel.super.getHeight() - this.height, this.y));
+                int height = IScrollPanel.super.getHeight();
+                if(IScrollPanel.this.horizontal != null) {
+                    height -= IScrollPanel.this.horizontal.getHeight();
+                }
+                this.y = Math.max(0, Math.min(height - this.height, this.y));
             }
         }
 
-        private void updateViewport(){
+        private void updateViewport() {
             final JViewport viewport = IScrollPanel.this.viewport;
             final Point viewPosition = viewport.getViewPosition();
             final Dimension target = IScrollPanel.this.content.getPreferredSize();
-            if(this.horizontal){
-                viewport.setViewPosition(new Point((int)((target.width - super.getWidth()) * this.x / (float)(super.getWidth() - this.width)), viewPosition.y));
+            if(this.horizontal) {
+                viewport.setViewPosition(new Point((int) ((target.width - super.getWidth()) * this.x / (float) (super.getWidth() - this.width)), viewPosition.y));
             } else {
-                viewport.setViewPosition(new Point(viewPosition.x, (int)((target.height - super.getHeight()) * this.y / (float)(super.getHeight() - this.height))));
+                viewport.setViewPosition(new Point(viewPosition.x, (int) ((target.height - super.getHeight()) * this.y / (float) (super.getHeight() - this.height))));
             }
         }
 
@@ -231,14 +242,16 @@ public class IScrollPanel extends IComponent {
             if(this.width == -1 || this.height == -1) {
                 return;
             }
-            if(this.hovered || this.scrolling){
+            if(this.backHover || this.scrolling){
                 g.setColor(IScrollPanel.SCROLLER_HOVER_BACKGROUND);
-                g.fillRect(0, 0, super.getWidth() - 1, super.getHeight() - 1);
 
-                g.setColor(IScrollPanel.SCROLLER_HOVER_COLOR);
-            } else {
-                g.setColor(IScrollPanel.SCROLLER_COLOR);
+                int width = super.getWidth() - 1;
+                if(this.horizontal && IScrollPanel.this.vertical != null){
+                    width -= IScrollPanel.this.vertical.getWidth();
+                }
+                g.fillRect(0, 0, width, super.getHeight() - 1);
             }
+            g.setColor(this.barHover || this.scrolling ? IScrollPanel.SCROLLER_HOVER_COLOR : IScrollPanel.SCROLLER_COLOR);
             g.fillRect(this.x, this.y, this.width, this.height);
         }
     }
